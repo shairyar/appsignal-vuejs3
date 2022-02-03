0fbf9d5fb021628aee0c0f7e2f888fba3f72015f
Shairyar Baig <shairyarbaig@gmail.com>
2022-02-03 11:48:44 +0500

    Initial AppSignal integration

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -43,6 +43,8 @@ import { Options, Vue } from 'vue-class-component';
 export default class HelloWorld extends Vue {
   msg!: string
 }
+
+throw new Error('Error');
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,19 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import Appsignal from "@appsignal/javascript";
+import { errorHandler } from "@appsignal/vue";
+import { plugin } from "@appsignal/plugin-window-events"
+
+const appsignal = new Appsignal({
+  key: "4c499a2b-ee1f-4e06-82d6-fb4267f9a214",
+});
+appsignal.use(plugin())
+
+const app = createApp(App).use(router).mount('#app')
+
+app.config = {
+  errorHandler: errorHandler(appsignal),
+};
+
+
