8d416017626baae92974dff01fefd40b589e620a
Shairyar Baig <shairyarbaig@gmail.com>
2022-02-03 13:07:21 +0500

    Code cleanup

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,11 @@ import { errorHandler } from "@appsignal/vue";
 import { plugin } from "@appsignal/plugin-window-events"
 
 const appsignal = new Appsignal({
-  key: "4c499a2b-ee1f-4e06-82d6-fb4267f9a214",
+  key: "FRONT-END-KEY",
 });
 appsignal.use(plugin())
 
-const app = createApp(App).use(router).mount('#app')
-
-app.config = {
-  errorHandler: errorHandler(appsignal),
-};
-
+const app = createApp(App)
+app.config.errorHandler = errorHandler(appsignal, app)
+app.use(router).mount('#app')
 
